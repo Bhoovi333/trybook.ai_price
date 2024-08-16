3c3e5fef09bdab51e94fecce114f0b23bda69d48
Bhoovi <138877556+Bhoovi333@users.noreply.github.com>
2024-08-16 09:07:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@
 import React from "react";
 import Table from "./components/table";
 import Aftertxt from "./components/after";
+import Toggle from ./components/toggle";
 function App(){
 return (
 <>
 <div className="section">
+  <Toggle />
 <h2>API Pricing</h2>
 <p>Our API pricing is based on the model used and the number of tokens processed. Here's a breakdown of the costs:</p>
 <Table />
